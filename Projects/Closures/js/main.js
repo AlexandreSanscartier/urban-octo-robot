08c2b785b736a069cf1fdf01912eed6d2640734b
AlexandreSanscartier <alexandre.sanscartier@codefor.ca>
2020-05-05 15:02:47 -0400

    Added closures

--- Projects/Closures/js/main.js
+++ Projects/Closures/js/main.js
@@ -27,52 +27,30 @@ function guessingGame(amount) {
     };
 }
 
-/*
-Write a function called arrayFrom which converts an array-like-object into an array.
-
-Examples:
-    var divs = document.getElementsByTagName('divs');
-    divs.reduce // undefined
-    var converted = arrayFrom(divs);
-    converted.reduce // function(){}....
-*/
-
 function arrayFrom(arrayLikeObject){
-    
+    return arraryLikeObjectArray = [].slice.call(arrayLikeObject);
 }
-
-/* 
-// Write a function called sumEvenArguments which takes all of the arguments passed to a function and returns the sum of the even ones.
-
-Examples:
-    sumEvenArguments(1,2,3,4) // 6
-    sumEvenArguments(1,2,6) // 8
-    sumEvenArguments(1,2) // 2
-*/
 
 function sumEvenArguments(){
-    
+    arrayFrom(arguments)
+    return arrayFrom(arguments).filter(function(value) {
+        return value % 2 === 0
+    }).map(function(value) {
+        return value * 2;
+    });
 }
 
-/* 
-Write a function called invokeMax which accepts a function and a maximum amount. invokeMax should return a function that when called increments a counter. If the counter is greater than the maximum amount, the inner function should return "Maxed Out"
-
-Examples:
-
-    function add(a,b){
-        return a+b
-    }
-
-    var addOnlyThreeTimes = invokeMax(add,3);
-    addOnlyThreeTimes(1,2) // 3
-    addOnlyThreeTimes(2,2) // 4
-    addOnlyThreeTimes(1,2) // 3
-    addOnlyThreeTimes(1,2) // "Maxed Out!"
-
-*/
-
 function invokeMax(fn, num){
-    
+    var max = num;
+    var amountsTried = 0;
+    return function isMaxedOut() {
+        if(amountsTried >= max) {
+            return "Maxed out";
+        } else {
+            amountsTried++;
+            return fn(arguments[0], arguments[1]);
+        }
+    }
 }
 
 /* 
